4d4a6eed953ab35267b013fe7084574eac2fca68
Robu Darius <robu.i.darius@gmail.com>
2025-11-14 21:08:12 +0200

    Initial Commit

---
+++ Classlly/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Classlly
+//
+//  Created by Robu Darius on 14.11.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ClassllyTests/ClassllyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClassllyTests.swift
+//  ClassllyTests
+//
+//  Created by Robu Darius on 14.11.2025.
+//
+
+import Testing
+@testable import Classlly
+
+struct ClassllyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
